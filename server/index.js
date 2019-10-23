ce7944bffbb962b258944a8c169243848464ecf5
Matthias Fore <matthiasfore@gmail.com>
2019-10-22 19:30:11 -0700

    1st render

---
+++ server/index.js
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+
+var express= require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = "3010";
+app.use(express.static(path.resolve(__dirname, '../client/')));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log("Now running on : ", PORT))
