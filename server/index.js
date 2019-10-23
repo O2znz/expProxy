c585ba087bff970354f9b68457b09ad9f30f2d8b
Matthias Fore <matthiasfore@gmail.com>
2019-10-22 21:12:31 -0700

    2 render but only one stylized

--- server/index.js
+++ server/index.js
@@ -1,8 +1,3 @@
-/*
-
-*/
-
-
 var express= require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
@@ -10,6 +5,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = "3010";
 app.use(express.static(path.resolve(__dirname, '../client/')));
+// app.use('/:listingid', express.static(path.resolve(__dirname, '../client/')));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log("Now running on : ", PORT))
