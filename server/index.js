9f1360297f38d3d479d4555792fb58cdc6943a13
Matthias Fore <matthiasfore@gmail.com>
2019-10-23 19:13:05 -0700

    Everyone is looking good

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = "3010";
-app.use(express.static(path.resolve(__dirname, '../client/')));
-// app.use('/:listingid', express.static(path.resolve(__dirname, '../client/')));
+// app.use(express.static(path.resolve(__dirname, '../client/')));
+app.use('/:listingid', express.static(path.resolve(__dirname, '../client/')));
 app.use(bodyParser.json());
+
+app.get("/:listingid", (req, res) => {
+    res.end();
+})
 
 app.listen(PORT, () => console.log("Now running on : ", PORT))
